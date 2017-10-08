8882ca347adc495dcc8a6cf52ccf2207ddab98fa
eacthulu <emilyalexandertech@gmail.com>
2017-10-08 10:26:35 -0700

    homescreen with subscreens, working. needs better placeholders

--- HackAZ/app/src/main/java/com/example/hackaz/MainActivity.java
+++ HackAZ/app/src/main/java/com/example/hackaz/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.hackaz;
 
 import android.app.Activity;
-import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,12 +14,14 @@ public class MainActivity extends Activity {
 
     private ArrayList<String> pages;
     ListView listView;
+    private int savePos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        savePos = 0;
         pages = new ArrayList<>();
         pages.add("Schedule");
         pages.add("Map");
@@ -31,15 +33,34 @@ public class MainActivity extends Activity {
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, pages);
         listView.setAdapter(adapter);
-        
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Log.i("Info", position+"");
+                savePos = position;
+                navSubPage();
             }
         });
+    }
+
+    private void navSubPage(){
+        if(savePos == 0){
+            Intent intent = new Intent(this, ScheduleActivity.class);
+            startActivity(intent);
+        }
+        else if(savePos == 1){
+            Intent intent = new Intent(this, MapActivity.class);
+            startActivity(intent);
+        }
+        else if(savePos == 2){
+            Intent intent = new Intent(this, MentorActivity.class);
+            startActivity(intent);
+        }
+        else{
+            Intent intent = new Intent(this, LiveStreamActivity.class);
+            startActivity(intent);
+        }
     }
 }
 
---
+++ HackAZ/app/src/main/java/com/example/hackaz/MentorActivity.java
@@ -0,0 +1,13 @@
+package com.example.hackaz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MentorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mentor);
+    }
+}
