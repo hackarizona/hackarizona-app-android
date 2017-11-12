b337f47c63b166711936a3ca0124b254a472f091
eacthulu <emilyalexandertech@gmail.com>
2017-11-11 17:57:39 -0700

    push notifications can be sent from firebase console to my virtual device. apologies for pushing an app within an app, git repository structure should be fixed now

---
+++ app/src/main/java/com/example/hackaz/MyFirebaseInstanceIDService.java
@@ -0,0 +1,33 @@
+package com.example.hackaz;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by Emily the Destroyer on 11/11/2017.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String refreshedToken) {
+        // Add custom implementation, as needed.
+
+    }
+
+}
